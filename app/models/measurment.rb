84526b65c4e56aacee297fd7595f378c25fd041c
Addis Belete <addis.bel@gmail.com>
2021-08-10 11:22:07 +0300

    Add assocations and validate presence

--- app/models/measurment.rb
+++ app/models/measurment.rb
@@ -1,2 +1,4 @@
 class Measurment < ApplicationRecord
+  has_many :results, dependent: :destroy
+  validates_presence_of :category
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,3 +1,4 @@
 class Result < ApplicationRecord
   belongs_to :measurment
+  validates_presence_of :result
 end
