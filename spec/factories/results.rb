68ba3616de1b4c188214bbcf919f349e536db389
Addis Belete <addis.bel@gmail.com>
2021-08-10 14:28:04 +0300

    Add routes

--- spec/factories/results.rb
+++ spec/factories/results.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :result do
     result { Faker::Number.number(10) }
-    measurment_id nil
+    measurment_id { nil }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,13 +1,13 @@
-require 'database_cleaner'
+require "database_cleaner"
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
 
-require File.expand_path('../config/environment', __dir__)
+require File.expand_path("../config/environment", __dir__)
 
 # Prevent database truncation if the environment is production
-abort('The Rails environment is running in production mode!') if Rails.env.production?
-require 'rspec/rails'
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -55,5 +55,13 @@ RSpec.configure do |config|
       example.run
     end
   end
+  # [...]
+end
+# [...]
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+# [...]
+RSpec.configuration do |config|
+  # [...]
+  config.include RequestSpecHelper, type: :request
   # [...]
 end
