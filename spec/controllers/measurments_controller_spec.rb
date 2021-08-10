2f11609a6f29d801735f63eb6e33f2289aca2420
Addis Belete <addis.bel@gmail.com>
2021-08-10 13:22:59 +0300

    Add more test for get request

--- spec/controllers/measurments_controller_spec.rb
+++ spec/controllers/measurments_controller_spec.rb
@@ -13,4 +13,25 @@ RSpec.describe Measurments API, type: :request do
       expect(response).to have_http_status(200)
     end
   end
+  describe "GET /measurments/:id" do
+    before { get '/measurments/#{measurment_id}' }
+    context "when the record exists" do
+      it "returns the measurment" do
+        expect(json).not_to be_empty
+        expect(json["id"]).to eq(measurment_id)
+      end
+      it "returns status code 200 " do
+        expect (response).to have_http_status(200)
+      end
+    end
+    context "when the record does not exist " do
+      let (:measurment_id) { 100 }
+      it "returns status code 404" do
+        expect(respones).to have_http_status(404)
+      end
+      it "returns a not found message" do
+        expect(response.body).to match(/Couldn't find Measurment/)
+      end
+    end
+  end
 end
