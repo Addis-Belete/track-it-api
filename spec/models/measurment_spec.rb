673be73b4238e414512130c637665daba4f3bf04
Addis Belete <addis.bel@gmail.com>
2021-08-10 11:13:49 +0300

    Add some test to Measurment model

--- spec/models/measurment_spec.rb
+++ spec/models/measurment_spec.rb
@@ -1,5 +1,6 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Measurment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:results).dependent(:destroy) }
+  it { should validate_presence_of(:category) }
 end
