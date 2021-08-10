2908e7dffaf874f4f32bc47579faa6a8d57a1dab
Addis Belete <addis.bel@gmail.com>
2021-08-10 13:10:01 +0300

    Add test for measurment Get request

--- spec/controllers/measurments_controller_spec.rb
+++ spec/controllers/measurments_controller_spec.rb
@@ -1,4 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe MeasurmentsController, type: :controller do
+RSpec.describe Measurments API, type: :request do
+  let!(:measurments) { create_list(:measurment, 10) }
+  let!(measurment_id) { measurments.first.id }
+  describe "GET /measurments" do
+    before { get "/measurments" }
+    it "returns measurments" do
+      expect(json).not_to be_empty
+      expect(json.size).to eq(10)
+    end
+    it "returns status code 200" do
+      expect(response).to have_http_status(200)
+    end
+  end
 end
