bdae029c08bd9964aa3b4992dd95b2d19c2ae4de
Addis Belete <addis.bel@gmail.com>
2021-08-24 11:27:45 +0300

    Add user_id column to measurments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :measurments, foreign_key: :user_id
+  validate :name, :email, :password_digest, presence: true
 end
---
+++ db/migrate/20210824082447_add_user_id_to_measurments.rb
@@ -0,0 +1,5 @@
+class AddUserIdToMeasurments < ActiveRecord::Migration[6.1]
+  def change
+    add_column :measurments, :user_id, :integer
+  end
+end
