952b9ac930b1dd4541afcb86066e4bb69c1ee9df
Addis Belete <addis.bel@gmail.com>
2021-08-24 16:32:45 +0300

    Add a test for valid credentials

--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe AuthorizeApiRequest do
   let(:user) { create(:user) }
   let(:header) { { "Authorization" => token_generator(user.id) } }
   subject(:invalid_request_obj) { described_class.new({}) }
-  subject(:request_obj) { describe_class.new(header) }
+  subject(:request_obj) { described_class.new(header) }
 
   describe "call" do
     context "when valid request" do
@@ -16,34 +16,36 @@ RSpec.describe AuthorizeApiRequest do
     # returns error message when invalid request
     context "when invalid request" do
       context "it raises a MissingToekn error" do
-        expect { invalid_request_obj.call }.to raise_error(ExceptionHandler::MissingToken, "Missing token")
+        it "raises a MissingToken error " do
+          expect { invalid_request_obj.call }.to raise_error(ExceptionHandler::MissingToken, "Missing token")
+        end
       end
-    end
-    context "when invalid token" do
-      subject(:invalid_request_obj) do
-        described_class.new("Authorization" => token_generator(5))
+      context "when invalid token" do
+        subject(:invalid_request_obj) do
+          described_class.new("Authorization" => token_generator(5))
+        end
+        it "raises an InvalidToken error" do
+          expect { invalid_request_obj.call }.to raise_error(ExceptionHandler::InvalidToken, /Invalid token/)
+        end
       end
-      it "raises an InvalidToken error" do
-        expect { invalid_request_obj.call }.to raise_error(ExceptionHandler::InvalidToken, /Invalid token/)
-      end
-    end
 
-    context "when token is expired" do
-      let(:header) { { "Authorization" => expired_token_generator(user.id) } }
-      subject(:request_obj) { described_class.new(header) }
-      it "raises ExceptionHandler::ExpiredSignature error" do
-        expect { request_obj.call }.to raise_error(
-          ExceptionHandler::InvalidToken, /Signature has expired/
-        )
+      context "when token is expired" do
+        let(:header) { { "Authorization" => expired_token_generator(user.id) } }
+        subject(:request_obj) { described_class.new(header) }
+        it "raises ExceptionHandler::ExpiredSignature error" do
+          expect { request_obj.call }.to raise_error(
+            ExceptionHandler::InvalidToken, /Signature has expired/
+          )
+        end
       end
-    end
-    context "fake token" do
-      let(:header) { { "Authorization" => "foobar" } }
-      subject(:invalid_request_obj) { described_class.new(header) }
-      it "handles JWT::DecodeError" do
-        expect { invalid_request_obj.call }.to raise_error(
-          ExceptionHandler::InvalidToken, /Not enough or too many segments/
-        )
+      context "fake token" do
+        let(:header) { { "Authorization" => "foobar" } }
+        subject(:invalid_request_obj) { described_class.new(header) }
+        it "handles JWT::DecodeError" do
+          expect { invalid_request_obj.call }.to raise_error(
+            ExceptionHandler::InvalidToken, /Not enough or too many segments/
+          )
+        end
       end
     end
   end
