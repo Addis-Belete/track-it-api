dd03608f6d60ea249c397285e4ae4737924b7a01
Addis Belete <addis.bel@gmail.com>
2021-08-25 08:09:26 +0300

    Add set_measurment private method

--- app/controllers/measurments_controller.rb
+++ app/controllers/measurments_controller.rb
@@ -1,4 +1,6 @@
 class MeasurmentsController < ApplicationController
+  before_action :set_measurment, only: [:show, :update, :destroy]
+
   def index
     @measurments = Measurment.all
     json_response(@measurments)
@@ -10,18 +12,15 @@ class MeasurmentsController < ApplicationController
   end
 
   def show
-    @measurment = Measurment.find(params[:id])
     json_response(@measurment)
   end
 
   def update
-    @measurment = Measurment.find(params[:id])
     @measurment.update(measurments_params)
     head :no_content
   end
 
   def destroy
-    @measurment = Measurment.find(params[:id])
     @measurment.destroy
     head :no_content
   end
@@ -30,5 +29,9 @@ class MeasurmentsController < ApplicationController
 
   def measurments_params
     params.permit(:category)
+  end
+
+  def set_measurment
+    @measurment = Measurment.find(params[:id])
   end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe ApplicationController, type: :controller do
-  let!(user) { create(:user) }
+  let!(:user) { create(:user) }
   let(:headers) { { "Authorization" => token_generator(user.id) } }
   let (:invalid_headers) { { "Authorization" => nil } }
 
